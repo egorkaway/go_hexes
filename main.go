e70602aea21ef8ff0fb408f56be591ec0dd72fdb
Egor Kunovsky <egorka@gmail.com>
2024-08-05 13:36:53 +0000

    aug 5 eve

--- main.go
+++ main.go
@@ -1,22 +1,16 @@
 package main
 
 import (
-    "bytes"
     "context"
     "encoding/json"
     "fmt"
     "io"
-    "io/ioutil"
     "log"
     "net/http"
     "os"
-    "path/filepath"
-    "time"
 
     "cloud.google.com/go/storage"
     "github.com/joho/godotenv"
-    h3 "github.com/uber/h3-go/v3"
-    "github.com/go-co-op/gocron"
 )
 
 const (
@@ -27,11 +21,6 @@ var (
     bucketName = "" // Bucket name will be fetched dynamically
     token      = "" // New token for authentication
 )
-
-type H3Data struct {
-    H3Index string `json:"h3_index"`
-    Visits  int    `json:"visits"`
-}
 
 func loadEnv() {
     if err := godotenv.Load(); err != nil {
@@ -71,7 +60,7 @@ func fetchToken() (string, error) {
     }
     defer resp.Body.Close()
 
-    body, err := ioutil.ReadAll(resp.Body)
+    body, err := io.ReadAll(resp.Body)
     if err != nil {
         return "", err
     }
@@ -101,7 +90,7 @@ func serveFromGCS(w http.ResponseWriter, r *http.Request) {
 
     bucketName, err := fetchDefaultBucket()
     if err != nil {
-        http.Error(w, "Failed to fetch default bucket ID", http.StatusInternalServerError)
+        http.Error(w, "Failed to bucket ID", http.StatusInternalServerError)
         log.Printf("Failed to fetch default bucket ID: %v", err)
         return
     }
@@ -123,175 +112,6 @@ func serveFromGCS(w http.ResponseWriter, r *http.Request) {
     }
 
     log.Printf("Successfully served %s from bucket %s", objectName, bucketName)
-}
-
-func fetchTemperature(lat, lon float64) (float64, error) {
-    const openWeatherMapAPIKey = "e7e06f3f2654e34e138f3d09ea001917"
-
-    url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%s&units=metric", lat, lon, openWeatherMapAPIKey)
-    resp, err := http.Get(url)
-    if err != nil {
-        return 0, err
-    }
-    defer resp.Body.Close()
-
-    var result map[string]interface{}
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return 0, err
-    }
-    if err := json.Unmarshal(body, &result); err != nil {
-        return 0, err
-    }
-
-    main, ok := result["main"].(map[string]interface{})
-    if !ok {
-        return 0, fmt.Errorf("invalid response format")
-    }
-
-    temp, ok := main["temp"].(float64)
-    if !ok {
-        return 0, fmt.Errorf("invalid temperature data")
-    }
-
-    return temp, nil
-}
-
-type GeoJSONFeature struct {
-    Type       string            `json:"type"`
-    Geometry   GeoJSONGeometry   `json:"geometry"`
-    Properties map[string]interface{} `json:"properties"`
-}
-
-type GeoJSONGeometry struct {
-    Type        string          `json:"type"`
-    Coordinates [][][]float64   `json:"coordinates"`
-}
-
-func generateGeoJSONFeature(h3cell string, temperature float64) (GeoJSONFeature, error) {
-    cellIndex := h3.FromString(h3cell)
-    cellBoundary := h3.ToGeoBoundary(cellIndex)
-
-    coordinates := make([][]float64, len(cellBoundary)+1)
-    for i, coord := range cellBoundary {
-        coordinates[i] = []float64{coord.Longitude, coord.Latitude}
-    }
-    coordinates[len(cellBoundary)] = coordinates[0] // Close the polygon
-
-    return GeoJSONFeature{
-        Type: "Feature",
-        Geometry: GeoJSONGeometry{
-            Type:        "Polygon",
-            Coordinates: [][][]float64{coordinates},
-        },
-        Properties: map[string]interface{}{
-            "h3cell":      h3cell,
-            "temperature": temperature,
-        },
-    }, nil
-}
-
-func fetchWeatherDataForH3Cells(h3Data []H3Data, outputPath string) error {
-    features := make([]GeoJSONFeature, 0, len(h3Data))
-
-    for _, data := range h3Data {
-        log.Printf("Fetching weather data for H3 cell: %s", data.H3Index)
-
-        cellCenter := h3.ToGeo(h3.FromString(data.H3Index))
-
-        temp, err := fetchTemperature(cellCenter.Latitude, cellCenter.Longitude)
-        if err != nil {
-            log.Printf("Failed to fetch temperature data for cell %s: %v", data.H3Index, err)
-            temp = 0
-        }
-
-        feature, err := generateGeoJSONFeature(data.H3Index, temp)
-        if err != nil {
-            return err
-        }
-
-        features = append(features, feature)
-    }
-
-    outputGeoJSON := map[string]interface{}{
-        "type":     "FeatureCollection",
-        "features": features,
-    }
-
-    var buf bytes.Buffer
-    encoder := json.NewEncoder(&buf)
-    encoder.SetEscapeHTML(false)
-    if err := encoder.Encode(outputGeoJSON); err != nil {
-        return err
-    }
-
-    if err := ioutil.WriteFile(outputPath, buf.Bytes(), 0644); err != nil {
-        return err
-    }
-
-    return nil
-}
-
-func uploadToObjectStorage(filename, bucketName, token string) error {
-    if bucketName == "" {
-        return fmt.Errorf("bucket name is empty")
-    }
-
-    fileData, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return err
-    }
-
-    url := fmt.Sprintf("https://storage.googleapis.com/upload/storage/v1/b/%s/o?uploadType=media&name=%s", bucketName, filepath.Base(filename))
-    fmt.Printf("Uploading to URL: %s\n", url) // Debugging line to confirm URL
-
-    req, err := http.NewRequest("POST", url, bytes.NewReader(fileData))
-    if err != nil {
-        return err
-    }
-    req.Header.Set("Authorization", "Bearer "+token)
-    req.Header.Set("Content-Type", "application/octet-stream")
-
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        return err
-    }
-    defer resp.Body.Close()
-
-    if resp.StatusCode != http.StatusOK {
-        bodyBytes, _ := ioutil.ReadAll(resp.Body)
-        return fmt.Errorf("failed to upload file: %s", string(bodyBytes))
-    }
-
-    return nil
-}
-
-func updateH3Level7() {
-    jsonFilename := filepath.Join(exportDir, "h3_level_7.json")
-    fileData, err := ioutil.ReadFile(jsonFilename)
-    if err != nil {
-        log.Fatalf("Failed to read JSON file for level 7: %v", err)
-    }
-
-    var h3Data []H3Data
-    err = json.Unmarshal(fileData, &h3Data)
-    if err != nil {
-        log.Fatalf("Failed to unmarshal JSON data for level 7: %v", err)
-    }
-
-    geoJSONFilename := filepath.Join(exportDir, "h3_level_7.geojson")
-    err = fetchWeatherDataForH3Cells(h3Data, geoJSONFilename)
-    if err != nil {
-        log.Fatalf("Failed to generate GeoJSON file for level 7: %v", err)
-    }
-
-    err = uploadToObjectStorage(geoJSONFilename, bucketName, token)
-    if err != nil {
-        log.Fatalf("Failed to upload GeoJSON file to Object Storage for level 7: %v", err)
-    }
-
-    log.Print("Successfully processed level 7, uploaded GeoJSON file to Object Storage")
 }
 
 func main() {
@@ -318,11 +138,6 @@ func main() {
     if err := os.MkdirAll(exportDir, os.ModePerm); err != nil {
         log.Fatal("Failed to create export directory:", err)
     }
-
-    // Schedule the update every 6 hours
-    s := gocron.NewScheduler(time.UTC)
-    s.Every(6).Hours().Do(updateH3Level7)
-    s.StartAsync()
 
     http.HandleFunc("/h3_level_7.geojson", serveFromGCS)
 
